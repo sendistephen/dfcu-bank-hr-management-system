73678779a1d53a6cc3f234cb07432d5180976915
sendistephen <sendi.stev@gmail.com>
2024-10-09 08:17:21 +0300

    feat: Wrap StaffForm in Suspense for proper handling of useSearchParams

--- frontend/app/staff/new/page.tsx
+++ frontend/app/staff/new/page.tsx
@@ -1,7 +1,18 @@
 import StaffForm from '@/components/staff-form';
+import { Loader2 } from 'lucide-react';
+import { Suspense } from 'react';
 
 const StaffRegisterForm = () => {
-  return <StaffForm />;
+  return (<Suspense
+    fallback={
+      <div className="flex justify-center items-center h-screen">
+        <Loader2 className="h-8 w-8 animate-spin text-gray-500" />
+      </div>
+    }
+  >
+    <StaffForm />
+  </Suspense>
+  )
 };
 
 export default StaffRegisterForm;
